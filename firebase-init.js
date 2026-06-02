001f61638eb35b8fc3606c78e9a932297fa6f92b
aajacky3254-netizen <aajacky3254@gmail.com>
2026-06-02 15:40:01 +0800

    修正後的 firebase-init.js 結尾

--- firebase-init.js
+++ firebase-init.js
@@ -16,5 +16,3 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-// firebase-init.js 結尾加上這兩行
-export { db, auth };
