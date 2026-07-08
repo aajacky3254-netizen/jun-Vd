2c7e767a921871fd39b4308f565a49de349462bd
aajacky3254-netizen <aajacky3254@gmail.com>
2026-07-08 20:10:42 +0800

    3

--- firebase-init.js
+++ firebase-init.js
@@ -4,12 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAnhhaiWXn5KFtU1YdB0oR_S9E0qhAlJuI",
-    authDomain: "jun-bus-dispatch.firebaseapp.com",
-    projectId: "jun-bus-dispatch",
-    storageBucket: "jun-bus-dispatch.firebasestorage.app",
-    messagingSenderId: "621725744717",
-    appId: "1:621725744717:web:64e663648d8c76b9bd6604"
+  apiKey: "AIzaSyCOEygVwCJbRLG68mJcN6MHu6YvgWkAPOc",
+  authDomain: "jun-bus-vd.firebaseapp.com",
+  projectId: "jun-bus-vd",
+  storageBucket: "jun-bus-vd.firebasestorage.app",
+  messagingSenderId: "984462733556",
+  appId: "1:984462733556:web:c8bf64415ea171eca6aea1",
+  measurementId: "G-6RYLKZHBZ3"
 };
 
 // 初始化並導出，讓其他檔案可以直接引用
